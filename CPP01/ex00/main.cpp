1e4f95db1f30fc9cd301a9181613ae3d20d9f717
Igor De spirlet <ide-spir@fu-r6-p6.s19.be>
2023-03-23 11:59:19 +0100

    Changement dans le makefile

--- CPP01/ex00/main.cpp
+++ CPP01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ide-spir <ide-spir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 13:55:19 by ide-spir          #+#    #+#             */
-/*   Updated: 2023/03/21 14:19:16 by ide-spir         ###   ########.fr       */
+/*   Updated: 2023/03/23 11:56:41 by ide-spir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,5 @@ int	main()
 	zombie1->annonce();
 
 	randomChump("Ronaldo");
-	randomChump("7");
-	randomChump("6");
-	randomChump("5");
-	randomChump("4");
-	randomChump("3");
-	randomChump("2");
-	randomChump("1");
-	randomChump("0");
-	randomChump("-1");
-	randomChump("-2");
 	delete zombie1;
 }
