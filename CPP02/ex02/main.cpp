528f65bd7fc9070ca97b493bdfe06ed06414f949
Igor De spirlet <ide-spir@shi-r2-p2.s19.be>
2023-04-03 19:09:14 +0200

    correction Cpp00 + CPP02 ex02

---
+++ CPP02/ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ide-spir <ide-spir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/03 15:34:29 by ide-spir          #+#    #+#             */
+/*   Updated: 2023/04/03 15:39:56 by ide-spir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int	main( void ) {
+	Fixed		a;
+	Fixed const	b( Fixed( 5.05f ) * Fixed( 2 ) );
+
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+
+	std::cout << b << std::endl;
+
+	std::cout << Fixed::max( a,b ) << std::endl;
+
+	return 0;
+}
