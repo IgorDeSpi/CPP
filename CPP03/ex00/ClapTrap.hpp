1ae961ba323ebb678fc4e3b1a8399e2f299a64d8
Igor De spirlet <ide-spir@fu-c3-p6.s19.be>
2023-04-06 23:40:38 +0200

    ex00 CPP 03

--- CPP03/ex00/ClapTrap.hpp
+++ CPP03/ex00/ClapTrap.hpp
@@ -6,7 +6,46 @@
 /*   By: ide-spir <ide-spir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 14:57:04 by ide-spir          #+#    #+#             */
-/*   Updated: 2023/04/06 14:57:05 by ide-spir         ###   ########.fr       */
+/*   Updated: 2023/04/06 22:38:55 by ide-spir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+
+class ClapTrap
+{
+    private:
+        std::string _name;
+        int         _hitPoints;
+        int         _energyPoints;
+        int         _attackDamage;
+
+    public:
+        ClapTrap(void);
+        ClapTrap(ClapTrap const &other);
+        ClapTrap    &operator=(ClapTrap const &other);
+        ~ClapTrap();
+
+        ClapTrap(std::string const &name);
+
+        void    setName(std::string const &name);
+        void    setHitPoints(int const hitPoints);
+        void    setEnergyPoints(int const energyPoints);
+        void    setAttackDamage(int const attackDamage);
+
+        const std::string   &getName() const;
+        int                 getHitPoints() const;
+        int                 getEnergyPoints() const;
+        int                 getAttackDamage() const;
+
+        void    attack(const std::string& target);
+        void    takeDamage(unsigned int amount);
+        void    beRepaired(unsigned int amount);
+};
+
+std::ostream    &operator<<(std::ostream &o, ClapTrap const &rhs);
+
+#endif
--- CPP03/ex00/main.cpp
+++ CPP03/ex00/main.cpp
@@ -3,10 +3,70 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ide-spir <ide-spir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ide-spir <narvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 14:57:11 by ide-spir          #+#    #+#             */
-/*   Updated: 2023/04/06 14:57:11 by ide-spir         ###   ########.fr       */
+/*   Updated: 2023/04/06 23:25:50 by ide-spir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
+
+int	main(void)
+{
+	ClapTrap clap("Hubert");
+
+	clap.attack("Francis");
+	clap.attack("Francis");
+	clap.attack("Francis");
+	clap.attack("Francis");
+	clap.attack("Francis");
+	clap.attack("Francis");
+	clap.attack("Francis");
+	clap.attack("Francis");
+	clap.attack("Francis");
+	clap.attack("Francis");
+	clap.attack("Francis");
+	clap.attack("Francis");
+	clap.setEnergyPoints(10);
+	clap.takeDamage(5);
+	clap.takeDamage(5);
+	clap.takeDamage(5);
+	clap.setHitPoints(10);
+	clap.takeDamage(5);
+	clap.beRepaired(5);
+
+
+	ClapTrap clap1("");
+
+	clap1.attack("Francis");
+	clap1.attack("");
+	clap1.attack("Francis");
+	clap1.attack("Francis");
+	clap1.attack("Francis");
+	clap1.attack("Francis");
+	clap1.setAttackDamage(10);
+	clap1.attack("Francis");
+	clap1.attack("Francis");
+	clap1.attack("Francis");
+	clap1.attack("Francis");
+	clap1.attack("Francis");
+	clap1.attack("Francis");
+	clap1.attack("Francis");
+	clap1.beRepaired(5);
+	clap1.setEnergyPoints(10);
+	clap1.takeDamage(12);
+	clap1.takeDamage(5);
+	clap1.beRepaired(5);
+	clap1.takeDamage(5);
+	clap1.setHitPoints(10);
+	clap1.takeDamage(5);
+	clap1.beRepaired(5);
+
+
+	ClapTrap clap2("");
+	clap2 = clap;
+	clap.attack("Francis");
+
+	return 0;
+}
