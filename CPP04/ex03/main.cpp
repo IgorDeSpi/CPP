3fb970e5486519954a804b76286b8279e947d069
Igor De spirlet <ide-spir@shi-r2-p2.s19.be>
2023-04-17 10:16:25 +0200

    From bob to Tom

--- CPP04/ex03/main.cpp
+++ CPP04/ex03/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ide-spir <narvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ide-spir <ide-spir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 15:09:52 by ide-spir          #+#    #+#             */
-/*   Updated: 2023/04/15 16:33:32 by ide-spir         ###   ########.fr       */
+/*   Updated: 2023/04/17 10:14:32 by ide-spir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,13 @@ int	main(void)
 	tmp = src->createMateria("cure");
 	me->equip(tmp);
 
-	ICharacter* bob = new Character("bob");
+	ICharacter* tom = new Character("Tom");
 
-	me->use(0, *bob);
-	me->use(1, *bob);
+	me->use(0, *tom);
+	std::cout << "*~~~~~~~~~~~~~~~~~~~~~~~~~~~*" << std::endl;
+	me->use(1, *tom);
 
-	delete bob;
+	delete tom;
 	delete me;
 	delete src;
 
