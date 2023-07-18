59bfb885a69a74a5fc38e12924d54ebecb9f8d65
Igor De spirlet <ide-spir@shi-r2-p2.s19.be>
2023-07-18 16:24:47 +0200

    start of ex02

---
+++ CPP09/ex02/PmergeMe.hpp
@@ -0,0 +1,182 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ide-spir <ide-spir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/18 15:14:30 by ide-spir          #+#    #+#             */
+/*   Updated: 2023/07/18 16:24:00 by ide-spir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PMERGEME_HPP
+# define PMERGEME_HPP
+
+# include <string>
+# include <iostream>
+# include <sstream>
+# include <vector>
+# include <deque>
+# include <algorithm>
+
+class	OverflowException : std::exception
+{
+	public:
+		virtual const char*	what() const throw();
+};
+
+std::vector<int>	FJAVector(std::vector<int> v);
+std::deque<int>		FJADeque(std::deque<int> d);
+int					parsing(int argc, char **argv, std::vector<int> &v, std::deque<int> &d);
+
+template	<typename T>
+void	ft_print(T &container)
+{
+	std::cout << "{ ";
+	for (size_t i = 0; i < container.size() - 1; i++)
+		std::cout << container[i] << ", ";
+		std::cout << container[container.size() - 1] << " ";
+		std::cout << "};" << std::endl;
+}
+
+//split
+template	<typename T>
+T	split(T &main, int leftover)
+{
+	T	pend;
+	typename T::iterator	it = main.begin();
+	size_t	halfSize = main.size() / 2;
+
+	while (main.size() > halfSize)
+	{
+		pend.push_back(*it);
+		it = main.erase(it);
+		it += 1;
+	}
+	if (leftover >= 0)
+		pend.push_back(leftover);
+	return (pend);
+}
+
+template	<typename T> // we assume that container size is even
+T	&makePairs(T &container)
+{
+	typename T::iterator	it = container.begin();
+
+	while (it != container.end())
+	{
+		if (*it > *(it + 1))
+			std::iter_swap(it, it + 1);
+		it += 2;
+	}
+	return (container);
+}
+
+template	<typename T>
+T	mergePairs(T &lhs, T &rhs) // we assume that container size is even
+{
+	T						res;
+	typename T::iterator	max_lhs;
+	typename T::iterator	max_rhs;
+
+	while (!(lhs.empty() && rhs.empty()))
+	{
+		max_lhs = std::max_element(lhs.begin(), lhs.end());
+		max_rhs = std::max_element(rhs.begin(), rhs.end());
+		if (rhs.empty() || (!lhs.empty() && *max_lhs > *max_rhs))
+		{
+			if (max_lhs + 1 != lhs.end() && *max_lhs == *(max_lhs + 1))
+				max_lhs++;
+			res.insert(res.begin(), max_lhs - 1, max_lhs + 1);
+			lhs.erase(max_lhs - 1, max_lhs + 1);
+		}
+		else
+		{
+			if (max_rhs + 1 != rhs.end() && *max_rhs == *(max_rhs + 1))
+				max_rhs++;
+			res.insert(res.begin(), max_rhs - 1, max_rhs + 1);
+			rhs.erase(max_rhs - 1, max_rhs + 1);
+		}
+	}
+	return (res);
+}
+
+template	<typename T>
+T	&orderPairs(T &container) // we assume that container size is even
+{
+	//We need to mergesort container on the 2n + 1 values so we have a container ordered by pairs size every 2n + 1 indexes are the max value of a pair
+	size_t	size = container.size();
+	if (size > 2) // division of container for the merge sort
+	{
+		size_t	split = (size / 2);
+		if (split % == 1)
+			--split;
+
+		//Split into 2 T
+		T	lhs(container.begin(), container.begin() + split);
+		T	rhs(container.begin() + split, container.end());
+
+		//Recursion to merge sort each split
+		lhs = orderPairs(lhs);
+		rhs = orderPairs(rhs);
+
+		//Merge
+		container = mergePairs(lhs, rhs);
+	}
+	return (container);
+}
+
+template	<typename T>
+T	insert(T &main, T &pend)
+{
+	std::vector<size_t>	toInsert;
+	size_t				size = pend.size();
+	size_t				i = 0;
+	size_t				jac = 1;
+	size_t				lastJac = 0;
+	size_t				lastLastJac = 0;
+
+	for (size_t j = 0; j < size; j++)
+		toInsert.push_back(j + 1);
+	while (i < size && jac <= size)
+	{
+		if (jac < lastJac) //Overflow protection
+			throw OverflowException();
+
+		for (size_t j = jac; j > lastJac; --j)
+		{
+			main.insert(std::lower_bound(main.begin(), main.begin() + jac + i - 1, pend[j - 1]), pend[j - 1]);
+			toInsert.erase(toInsert.begin() + jac - 1 - i);
+			i++;
+		}
+		lastLastJac = lastJac;
+		lastJac = jac;
+		jac = lastJac + 2*lastLastJac;
+	}
+	//add after jac
+	for(std::vector<size_t>::const_reverse_iterator it = toInsert.rbegin(); it != toInsert.rend(); it++)
+	{
+		main.insert(std::lower_bound(main.begin(), main.begin() + *it + i - 1, pend[*it - 1]), pend[*it - 1]);
+		i++;
+	}
+	return (main);
+}
+
+template	<typename ForwardIterator>
+bool	isSorted(ForwardIterator begin, ForwardIterator end)
+{
+	if (begin == end)
+		return true;
+
+	ForwardIterator	next = begin;
+	while (++next != end)
+	{
+		if (*next < *begin)
+			return false;
+		++begin;
+	}
+	return true;
+}
+
+#endif
